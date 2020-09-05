d3cbcf172828e76a04acbe6917a09598dab5a5bd
idonun <mc.natsu831@gmail.com>
2020-09-06 01:46:06 +0900

    [add] + FavoriteView

---
+++ CleanArchitectureExample/Presentation/View/Favorite/FavoriteView.swift
@@ -0,0 +1,21 @@
+//
+//  FavoriteView.swift
+//  CleanArchitectureExample
+//
+//  Created by NatsukiIdota on 2020/07/26.
+//  Copyright © 2020 idonuntius. All rights reserved.
+//
+
+import SwiftUI
+
+struct FavoriteView: View {
+    var body: some View {
+        Text("Favorite")
+    }
+}
+
+struct FavoriteView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteView()
+    }
+}
